9c48603b0d59fb5411d822acc20d7769772bef69
Dustin King <cathodion@gmail.com>
2019-06-28 06:27:01 -0400

    add subject awareness

--- botty.rb
+++ botty.rb
@@ -13,6 +13,8 @@ class Botty
     @friend=Friend.new
     @just_asked_name=false
     @just_got_name=false
+    @subjects={}
+    @current_subject=nil
   end
 
   def tell!(s)
@@ -21,6 +23,31 @@ class Botty
       @friend.name = s
       @just_got_name = true
       @just_asked_name = false
+
+    elsif (m = s.match /^(?<subject>\w+)\'s (?<property>\w+) is (?<value>.+)/i )
+      @current_subject = m[:subject]
+      if !@subjects[@current_subject]
+        @subjects[@current_subject] = {}
+      end
+      @subjects[@current_subject][m[:property]] = m[:value]
+      @response = "K."
+
+    elsif (m = s.match /^what is (?<subject>\w+)\'s (?<property>\w+)/i )
+      @current_subject = m[:subject]
+      @response = if !@subjects[m[:subject]]
+        "I'm sorry, I don't know what #{m[:subject]} is."
+      elsif !@subjects[m[:subject]][m[:property]]
+        "I don't know #{@current_subject}'s #{m[:property]}'."
+      else
+        "#{@current_subject}'s #{m[:property]} is #{@subjects[m[:subject]][m[:property]]}."
+      end
+
+    elsif s.match /what are we talking about/i
+      if @current_subject
+        @response = "We're talking about #{@current_subject}."
+      else
+        @response = "I don't know."
+      end
     end
   end
 
@@ -28,6 +55,12 @@ class Botty
     if @new
       @new = false
       "Hello, my name is #{@name}."
+
+    elsif @response
+      # This is bad.  It's just here for now.  Maybe.
+      output = @response
+      @response = nil
+      output
 
     elsif @just_got_name
       @just_got_name = false
@@ -38,7 +71,7 @@ class Botty
       "What is your name?"
 
     else
-      "you said #{@last_said}"
+      "You said '#{@last_said}'.  I don't know what that means."
     end
   end
 end
