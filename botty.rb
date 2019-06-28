b26d98b537c588dad761848181c7fea12c60431b
Dustin King <cathodion@gmail.com>
2019-06-28 04:34:46 -0400

    Move logic to Botty class

--- botty.rb
+++ botty.rb
@@ -1,7 +1,32 @@
+class Botty
+  def initialize(name="Botty")
+    @last_said = ''
+    @new = true
+    @name=name
+  end
+
+  def tell!(s)
+    @last_said = s
+  end
+
+  def response
+    if @new
+      @new = false
+      "Hello, my name is #{@name}."
+    else
+      "you said #{@last_said}"
+    end
+  end
+end
+
+botty = Botty.new
+
+puts "\n#{botty.response}\n\n"
+
 while true
   print ">>> "
   $stdout.flush
 
-  s = gets.strip
-  puts "\nyou said '#{s}'\n\n"
+  botty.tell! gets.strip
+  puts "\n#{botty.response}\n\n"
 end
